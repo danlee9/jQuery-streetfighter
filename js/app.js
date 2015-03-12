1a9ed0ced70d322ca7513d1d8e594e8333dd336a
unknown <dan.dh.lee@gmail.com>
2015-03-12 15:04:37 -0700

    Add Hadouken animation and sound

--- js/app.js
+++ js/app.js
@@ -6,5 +6,24 @@ $(document).ready(function() {
 	.mouseleave(function() {
 		$('.ryu-still').show();
 		$('.ryu-ready').hide();
+	})
+	.mousedown(function() {
+		playHadouken();
+		$('.ryu-ready').hide();
+		$('.ryu-throwing').show();
+		$('.hadouken').finish().show().animate({'left': '1020px'}, 500, function() {
+			$(this).hide();
+			$(this).css('left', '-180px');
+		});
+	}) 
+	.mouseup(function() {
+		//return ryu to ready position
+		$('.ryu-ready').show();
+		$('.ryu-throwing').hide();
 	});
 });
+function playHadouken() {
+	$('#hadouken-sound')[0].volume = 0.5;
+	$('#hadouken-sound')[0].load();
+	$('#hadouken-sound')[0].play();
+}
